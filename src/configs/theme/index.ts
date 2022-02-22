fbef5b01900a59339884de595531b90f101ea464
deniska <tervizator@gmail.com>
2022-02-22 15:55:25 +0400

    add theme

--- src/configs/theme/index.ts
+++ src/configs/theme/index.ts
@@ -1,10 +1,10 @@
 const theme = {
-    colors: {
-        black: '#1D1D1B',
-        white: '#FFFFFF',
-        yellow: '#E4F019',
-        grey: '#C2C2BC'
-    }
-}
+  colors: {
+    black: "#1D1D1B",
+    white: "#FFFFFF",
+    yellow: "#E4F019",
+    grey: "#C2C2BC",
+  },
+};
 
-export default theme
+export default theme;
